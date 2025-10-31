815882463f61c15a61d277736dcd41f8efe0b904
BrkyDrms <durmus.berkay.db@gmail.com>
2025-10-31 11:28:52 +0300

    etiya-project

--- common/src/main/java/com/etiya/common/filters/JwtAuthFilter.java
+++ common/src/main/java/com/etiya/common/filters/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,8 @@ import java.util.List;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+
+
 
     public JwtAuthFilter(JwtService jwtService) {
         this.jwtService = jwtService;
