c9544f236558ba802241ffe23a339180b50126a9
BrkyDrms <durmus.berkay.db@gmail.com>
2025-11-05 22:57:39 +0300

    catalogservice-begining

---
+++ catalogservice/src/main/java/com/etiya/catalogservice/domain/Product.java
@@ -0,0 +1,27 @@
+package com.etiya.catalogservice.domain;
+
+import com.etiya.common.entities.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private double price;
+}
