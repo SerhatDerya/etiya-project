76564a605c9846249e63546585a66cba1c26ae9b
BrkyDrms <durmus.berkay.db@gmail.com>
2025-11-07 17:30:38 +0300

    etiya-project

--- customerservice/src/main/java/com/etiya/customerservice/service/concretes/BillingAccountServiceImpl.java
+++ customerservice/src/main/java/com/etiya/customerservice/service/concretes/BillingAccountServiceImpl.java
@@ -26,47 +26,47 @@ public class BillingAccountServiceImpl implements BillingAccountService {
 
     @Override
     public CreatedBillingAccountResponse add(CreateBillingAccountRequest request) {
-//        BillingAccount billingAccount = BillingAccountMapper.INSTANCE.billingAccountFromCreateBillingAccountRequest(request);
-//        BillingAccount result = billingAccountRepository.save(billingAccount);
-//        BillingAccount fullAccount = billingAccountRepository.findByIdWithAccount(result.getId())
-//                .orElseThrow(() -> new RuntimeException("Status or Type missing"));
-//        CreateBillingAccountEvent  event = BillingAccountMapper.INSTANCE.createBillingAccountEventFromBillingAccount(fullAccount);
-//        createBillingAccountProducer.produceBillingAccountCreated(event);
-//        CreatedBillingAccountResponse response = BillingAccountMapper.INSTANCE.createdBillingAccountResponseFromBillingAccount(result);
-//        return  response;
-        BillingAccount billingAccount = new BillingAccount();
-        billingAccount.setAccountName(request.getAccountName());
-        Customer customer = new Customer();
-        customer.setId(request.getCustomerId());
-        billingAccount.setCustomer(customer);
-        Address address = new Address();
-        address.setId(request.getAddressId());
-        billingAccount.setAddress(address);
-        Status status = new Status();
-        status.setId(request.getStatusId());
-        billingAccount.setStatus(status);
-        Type type = new Type();
-        type.setId(request.getTypeId());
-        billingAccount.setType(type);
+        BillingAccount billingAccount = BillingAccountMapper.INSTANCE.billingAccountFromCreateBillingAccountRequest(request);
         BillingAccount result = billingAccountRepository.save(billingAccount);
         BillingAccount fullAccount = billingAccountRepository.findByIdWithAccount(result.getId())
-        .orElseThrow(() -> new RuntimeException("Status or Type missing"));
-
-        CreateBillingAccountEvent event = new CreateBillingAccountEvent(
-                fullAccount.getId().toString(),
-                fullAccount.getCustomer().getId().toString(),
-                fullAccount.getAddress().getId().toString(),
-                fullAccount.getStatus().getId().toString(),
-                fullAccount.getType().getId().toString(),
-                fullAccount.getAccountNumber(),
-                fullAccount.getAccountName(),
-                fullAccount.getType().getName(),
-                fullAccount.getStatus().getName());
+                .orElseThrow(() -> new RuntimeException("Status or Type missing"));
+        CreateBillingAccountEvent  event = BillingAccountMapper.INSTANCE.createBillingAccountEventFromBillingAccount(fullAccount);
         createBillingAccountProducer.produceBillingAccountCreated(event);
-        CreatedBillingAccountResponse response = new CreatedBillingAccountResponse();
-        response.setId(result.getId());
-        return response;
-
+        CreatedBillingAccountResponse response = BillingAccountMapper.INSTANCE.createdBillingAccountResponseFromBillingAccount(result);
+        return  response;
+//        BillingAccount billingAccount = new BillingAccount();
+//        billingAccount.setAccountName(request.getAccountName());
+//        Customer customer = new Customer();
+//        customer.setId(request.getCustomerId());
+//        billingAccount.setCustomer(customer);
+//        Address address = new Address();
+//        address.setId(request.getAddressId());
+//        billingAccount.setAddress(address);
+//        Status status = new Status();
+//        status.setId(request.getStatusId());
+//        billingAccount.setStatus(status);
+//        Type type = new Type();
+//        type.setId(request.getTypeId());
+//        billingAccount.setType(type);
+//        BillingAccount result = billingAccountRepository.save(billingAccount);
+//        BillingAccount fullAccount = billingAccountRepository.findByIdWithAccount(result.getId())
+//        .orElseThrow(() -> new RuntimeException("Status or Type missing"));
+//
+//        CreateBillingAccountEvent event = new CreateBillingAccountEvent(
+//                fullAccount.getId().toString(),
+//                fullAccount.getCustomer().getId().toString(),
+//                fullAccount.getAddress().getId().toString(),
+//                fullAccount.getStatus().getId().toString(),
+//                fullAccount.getType().getId().toString(),
+//                fullAccount.getAccountNumber(),
+//                fullAccount.getAccountName(),
+//                fullAccount.getType().getName(),
+//                fullAccount.getStatus().getName());
+//        createBillingAccountProducer.produceBillingAccountCreated(event);
+//        CreatedBillingAccountResponse response = new CreatedBillingAccountResponse();
+//        response.setId(result.getId());
+//        return response;
+//
 
 
 
